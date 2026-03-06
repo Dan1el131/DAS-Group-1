dd936a9f194ef084733170fb22d0246f4b2f4cb7
Dan1el131 <151350788+Dan1el131@users.noreply.github.com>
2026-03-06 12:20:47 +0000

    Add files via upload

---
+++ R_code.r
@@ -0,0 +1,6 @@
+library(tidyverse)    # Data wrangling 
+library(ggplot2)      # Data visualization
+library(performance)  # Model assessment
+library(skimr)        # Exploratory analysis
+library(sjPlot)       # Plot and tables for linear models
+library(kableExtra)   # Formatting html table
